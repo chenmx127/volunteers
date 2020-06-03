d55c2c1b74a19bc2408c5b945a2bf943aeb0d691
chenmx127 <62276324+chenmx127@users.noreply.github.com>
2020-06-03 18:15:29 +0800

    mx-4

--- pages/index1/index1.js
+++ pages/index1/index1.js
@@ -12,9 +12,13 @@ Page({
       {path:"../../img/hdzm.png",value:"活动招募"},
       {path:"../../img/zcfg.png",value:"政策法规"}],
     contentlist:[
-      {tit_img:"../../img/jr.png",title:"参与活动",img:"../../img/tu1.png",con_title:"为灾区人民模子捐款",address:"某某广场",hour:"2019年3月28日",person:"志愿者协会"},
-      {tit_img:"../../img/hd.png",title:"活动招募",img:"../../img/tupian.png",con_title:"为孤寡老人送爱心午餐",address:"某某广场",hour:"2019年3月28日",person:"志愿者协会"},
-      {tit_img:"../../img/zc.png",title:"政策法规",img:"../../img/tu1.png",con_title:"为灾区人民模子捐款",address:"某某广场",hour:"2019年3月28日",person:"志愿者协会"}]
+      {tit_img:"../../img/jr.png",title:"参与活动"},
+      {tit_img:"../../img/hd.png",title:"活动招募"},
+      {tit_img:"../../img/zc.png",title:"政策法规"}],
+    actlist:[
+      {img:"../../img/tu1.png",title:"为灾区人民模子捐款",address:"某某广场",hour:"2019年3月28日",person:"志愿者协会"},
+      {img:"../../img/tupian.png",title:"为孤寡老人送爱心午餐",address:"某某广场",hour:"2019年3月28日",person:"志愿者协会"},
+      {img:"../../img/tu1.png",title:"为灾区人民模子捐款",address:"某某广场",hour:"2019年3月28日",person:"志愿者协会"}]
   },
 
   /**
