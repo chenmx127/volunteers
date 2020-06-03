8494dbb5e3ec5928a76dd24f2604587acac3849b
chenmx127 <62276324+chenmx127@users.noreply.github.com>
2020-06-03 14:13:01 +0800

    mx-2

--- pages/index1/index1.js
+++ pages/index1/index1.js
@@ -1,18 +1,21 @@
 // pages/index1/index1.js
+let shouye = require('../../utils/api.js'); 
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    background: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad(options) {
+    shouye.TEER('/volunteer/activityList','GET',{},res=>{
+      console.log(res)
+    })
   },
 
   /**
