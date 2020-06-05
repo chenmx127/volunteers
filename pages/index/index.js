6dd849b9ead971e01a8957ec7d3f90b51955d744
chenmx127 <62276324+chenmx127@users.noreply.github.com>
2020-06-05 16:48:09 +0800

    mx-7

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,92 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
+// pages/index1/index1.js
+let shouye = require('../../utils/api.js'); 
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    background: ["../../img/banner1.jpg","../../img/banner2.jpg","../../img/banner3.jpg"],
+    imglist:[
+      {path:"../../img/cyhd.png",value:"参与活动"},
+      {path:"../../img/hdzm.png",value:"活动招募"},
+      {path:"../../img/zcfg.png",value:"政策法规"}],
+    contentlist:[
+      {tit_img:"../../img/jr.png",title:"参与活动",id:0},
+      {tit_img:"../../img/hd.png",title:"活动招募",id:1},
+      {tit_img:"../../img/zc.png",title:"政策法规",id:0}],
+    canjia:[],
+    zhao:[]
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+    shouye.TEER('/volunteer/website/canjiaList','GET',{
+      openId:"oiUyR4iykJCuGJOFg6t2IgGN0_xA",type:1,type1:1
+    },res=>{
+      console.log(res)
       this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
+        canjia:res.data.data.list
       })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
+    }),
+    shouye.TEER('/volunteer/website/canjiaList','GET',{
+      openId:"oiUyR4iykJCuGJOFg6t2IgGN0_xA",type:2,type1:1
+    },res=>{
+      console.log(res)
+      this.setData({
+        zhao:res.data.data.list
       })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
     })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
   }
 })
